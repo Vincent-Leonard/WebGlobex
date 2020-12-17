b6ced95a2d223f84ebbc4c6911acb51fa884bca0
Jonathan-Valentino <jonathanvalentinoprawira@yahoo.com>
2020-12-17 11:09:15 +0700

    Add Jaka model and migration

---
+++ app/Models/Jaka.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Jaka extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'jaka_name',
+    ];
+}
---
+++ database/migrations/2020_12_17_040540_add_jaka_id_to_lecturers_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddJakaIdToLecturersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('lecturers', function (Blueprint $table) {
+            $table->unsignedBigInteger('jaka_id')->index()->after('title_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('lecturers', function (Blueprint $table) {
+            $table->dropColumn('jaka_id');
+        });
+    }
+}
