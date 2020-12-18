7d901b4755c29c56aa52202dc4e5c094d7d49663
Jonathan-Valentino <jonathanvalentinoprawira@yahoo.com>
2020-12-18 08:12:39 +0700

    Update LoginController.php

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
@@ -36,5 +39,67 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+    public function login(Request $request)
+    {
+        $student = [
+            'email' => $request->email,
+            'password' => $request->password,
+            // 'role_id' => 1,
+            'is_login' => '0',
+            // 'is_active' => '1',
+            // 'is_verified' => '1',
+        ];
+
+        $lecturer = [
+            'email' => $request->email,
+            'password' => $request->password,
+            // 'role_id' => 2,
+            'is_login' => '0',
+            // 'is_active' => '1',
+            // 'is_verified' => '1',
+        ];
+
+        $staff = [
+            'email' => $request->email,
+            'password' => $request->password,
+            // 'role_id' => 3,
+            'is_login' => '0',
+            // 'is_active' => '1',
+            // 'is_verified' => '1',
+        ];
+
+        if(Auth::attempt($student)){
+            $this->isLogin(Auth::id());
+            return redirect()->route('event.index');
+        } elseif(Auth::attempt($lecturer)){
+            $this->isLogin(Auth::id());
+            return redirect()->route('event.index');
+        } elseif(Auth::attempt($staff)){
+            $this->isLogin(Auth::id());
+            return redirect()->route('event.index');
+        }
+
+        return redirect()->route('login');
+    }
+
+    public function logout(Request $request)
+    {
+        $user = User::findOrFail(Auth::id());
+        $user->update([
+            'is_login' => '0',
+        ]);
+
+        $request->session()->invalidate();
+        return $this->LoggedOut($request) ?: redirect('login');
+    }
+
+    public function isLogin(int $id)
+    {
+        $user = User::findOrFail($id);
+        return $user->update([
+            'is_login' => '1',
+        ]);
     }
 }
